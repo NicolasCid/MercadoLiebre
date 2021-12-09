1c132b99e60f808c630de5da5415c0a41d361902
NicolasCid <nicolass.cid@gmail.com>
2021-12-09 19:56:10 -0300

    Heroku

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//Definir un puerto
+app.set('port',process.env.PORT ||3050)
+//Definir un observador del puerto definido
+
+app.listen(app.get('port'), () => console.log("Listening on port:3050"));
+
+//Definir las rutas de mi servidor
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
